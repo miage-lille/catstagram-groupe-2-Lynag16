208bc16965a8e30d595ed303aa318185ec29d0c3
Agouazi Lynda <lynda.agouazi.etu@b13p23.fil.univ-lille.fr>
2025-02-10 13:16:49 +0100

    update the tp

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,39 +1,29 @@
-import { Loop, liftState } from 'redux-loop';
-import { compose } from 'redux';
-import { Actions } from './types/actions.type';
+type Increment = { type: 'INCREMENT' };
+type Decrement = { type: 'DECREMENT' };
 
-export type State = unknown; // TODO : Update this type !
+const increment = (): Increment => ({ type: 'INCREMENT' });
+const decrement = (): Decrement => ({ type: 'DECREMENT' });
 
-export const defaultState = {}; // TODO : Update this value !
+type Actions = Increment | Decrement;
 
-export const reducer = (state: State | undefined, action: Actions): State | Loop<State> => {
+export type State = {
+  counter: number;
+};
+
+export const defaultState: State = {
+  counter: 0
+};
+
+const reducer = (state: State | undefined, action: Actions): State => {
   if (!state) return defaultState; // mandatory by redux
   switch (action.type) {
     case 'INCREMENT':
-      throw 'Not Implemented';
+      return { ...state, counter: state.counter + 1 };
     case 'DECREMENT':
-      throw 'Not Implemented';
-    case 'SELECT_PICTURE':
-      throw 'Not Implemented';
-    case 'CLOSE_MODAL':
-      throw 'Not Implemented';
-    case 'FETCH_CATS_REQUEST':
-      throw 'Not Implemented';
-    case 'FETCH_CATS_COMMIT':
-      throw 'Not Implemented';
-    case 'FETCH_CATS_ROLLBACK':
-      throw 'Not Implemented';
+      return state.counter > 3 ? { ...state, counter: state.counter - 1 } : state;
+    default:
+      return state;
   }
 };
 
-export const counterSelector = (state: State) => {
-  throw 'Not Implemented';
-};
-export const picturesSelector = (state: State) => {
-  throw 'Not Implemented';
-};
-export const getSelectedPicture = (state: State) => {
-  throw 'Not Implemented';
-};
-
-export default compose(liftState, reducer);
+export { increment, decrement, reducer };
